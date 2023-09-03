f5406615d0872f9d3e2b5c6c570566515c694783
Marcos Sousa <antoniomarcus13@gmail.com>
2023-09-03 18:27:22 -0300

    ajuste no front, consumo da API com AXIOS

--- apps/public/static/main.js
+++ apps/public/static/main.js
@@ -1,119 +1,27 @@
-// // async function login() {
-// //     const emailInput = document.querySelector('input[name="email"]');
-// //     const passwordInput = document.querySelector('input[name="password"]');
-
-// //     // Verifique se o campo de e-mail não está vazio
-// //     if (!emailInput.value) {
-// //         alert('Preencha o campo de e-mail antes de fazer o login.');
-// //         return;
-// //     }
-
-// //     // Verifique se o email está em um formato válido usando uma expressão regular
-// //     const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
-// //     if (!emailRegex.test(emailInput.value)) {
-// //         alert('Insira um email válido antes de fazer o login.');
-// //         return;
-// //     }
-// //     try {
-// //         const response = await axios.post('http://localhost:8000/token', {
-// //             email: emailInput.value,
-// //             password: passwordInput.value,
-            
-// //         });
-
-// //         if (response.status === 200) {
-// //             // Login bem-sucedido, faça o redirecionamento ou a ação necessária
-// //             console.log('Login bem-sucedido');
-// //         }
-// //     } catch (error) {
-// //         console.error('Erro ao fazer login:', error);
-// //     }
-// // }
-
-// // // Adicione um evento de clique para chamar a função de login
-// // document.querySelector('.btn').addEventListener('click', login);
-
-
-
-// async function login() {
-//     const emailInput = document.querySelector('input[name="email"]');
-//     const passwordInput = document.querySelector('input[name="password"]');
-
-//     if (!emailInput.value) {
-//         alert('Preencha o campo de e-mail antes de fazer o login.');
-//         return;
-//     }
-
-//     const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
-//     if (!emailRegex.test(emailInput.value)) {
-//         alert('Insira um email válido antes de fazer o login.');
-//         return;
-//     }
-
-//     try {
-//         const response = await axios.post('http://localhost:8000/token', {
-//             email: emailInput.value,
-//             password: passwordInput.value,
-//         });
-
-//         if (response.status === 200) {
-//             const token = response.data.access_token;
-
-//             // Armazene o token em localStorage para uso posterior
-//             localStorage.setItem('token', token);
-
-//             // Redirecione ou faça outras ações necessárias após o login bem-sucedido
-//             console.log('Login bem-sucedido');
-//         }
-//     } catch (error) {
-//         console.error('Erro ao fazer login:', error);
-//     }
-// }
-
-// // Adicione um evento de clique para chamar a função de login
-// document.querySelector('.btn').addEventListener('click', login);
-
-
-async function login() {
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("form");
     const emailInput = document.querySelector('input[name="email"]');
     const passwordInput = document.querySelector('input[name="password"]');
-    const responseDiv = document.querySelector('.response');
+    const responseMessage = document.querySelector("#responseMessage");
 
-    if (!emailInput.value) {
-        alert('Preencha o campo de e-mail antes de fazer o login.');
-        return;
-    }
+    loginForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
 
-    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
-    if (!emailRegex.test(emailInput.value)) {
-        alert('Insira um email válido antes de fazer o login.');
-        return;
-    }
+        const email = emailInput.value;
+        const password = passwordInput.value;
 
-    try {
-        const response = await axios.post('http://localhost:8000/login', {
-            email: emailInput.value,
-            password: passwordInput.value,
-        });
-
-        if (response.status === 200) {
-            const token = response.data.access_token;
-
-            // Armazene o token em localStorage para uso posterior
-            localStorage.setItem('token', token);
-
-            // Exiba a resposta do back-end no front-end
-            responseDiv.textContent = 'Resposta do servidor: ' + JSON.stringify(response.data);
-
-            // Redirecione ou faça outras ações necessárias após o login bem-sucedido
-            console.log('Login bem-sucedido');
+        try {
+            const response = await axios.post("http://127.0.0.1:8000/token", { email, password });
+            const accessToken = response.data.acess_token; // Certifique-se de que o nome do campo esteja correto
+            // Armazene o token em um local seguro, como em um cookie ou LocalStorage
+            // Exemplo com LocalStorage:
+            localStorage.setItem("accessToken", accessToken);
+            responseMessage.innerText = "Login bem-sucedido!";
+            // Redirecione para outra página ou realize ações após o login
+            window.location.href = "user-page.html";
+        } catch (error) {
+            console.error(error);
+            responseMessage.innerText = "Falha no login. Verifique suas credenciais.";
         }
-    } catch (error) {
-        console.error('Erro ao fazer login:', error);
-        // Limpe o conteúdo da div de resposta em caso de erro
-        responseDiv.textContent = 'Erro ao fazer login.';
-    }
-}
-
-// Adicione um evento de clique para chamar a função de login
-document.querySelector('.btn').addEventListener('click', login);
+    });
+});
--- apps/src/server.py
+++ apps/src/server.py
@@ -9,6 +9,7 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
     "http://localhost:5050",
+    "http://127.0.0.1:5500"
 ]
 
 app.add_middleware(
